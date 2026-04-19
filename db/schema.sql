c2d17f8952918fd8ce665ff5a85b0c094cc1b885
kousei <kottus629@outlook.jp>
2026-04-19 16:59:10 +0900

    観光地コンテンツ　db

--- db/schema.sql
+++ db/schema.sql
@@ -29,6 +29,19 @@ create table visits (
         foreign key (prefecture_id) references prefectures(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
+create table tourist_spots (
+    id int primary key auto_increment,
+    prefecture_id int not null,
+    name varchar(255) not null,
+    `rank` int not null,
+    image_url varchar(255),
+    affiliate_url varchar(255),
+    created_at timestamp default current_timestamp,
+    key idx_prefecture_id (prefecture_id),
+    constraint fk_tourist_spots_prefectures
+        foreign key (prefecture_id) references prefectures(id)
+)Engine = InnoDB DEFAULT CHARSET = utf8mb4;
+
 insert into prefectures (id, name, region) values
     (1, '北海道', '北海道'),
     (2, '青森県', '東北'),
