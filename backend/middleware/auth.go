3fe70bbe15a8f39525e5ee99e316afa70245aa91
kosei matsuo <kottsu629@outlook.jp>
2026-05-12 12:14:55 +0000

    ミドルウェア実装

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -1,1 +1,29 @@
 package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func Auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// ここに処理を書く
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+	}
+}
