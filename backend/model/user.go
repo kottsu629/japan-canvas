d9b0f07461d31fc5a432722046b9fd992d8594c5
kosei matsuo <kottsu629@outlook.jp>
2026-05-14 20:18:52 +0000

    未使用ファイル削除

--- backend/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"-"`
-	CreatedAt    string `json:"created_at"`
-}
