5faf3de24471e6ee025616611044b27209c2e80e
kosei matsuo <kottsu629@outlook.jp>
2026-05-26 11:08:36 +0000

    不用物　削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,3 @@ export const metadata: Metadata = {
   title: "japan-canvas",
   description: "日本を旅行しながらキャンバスを描こう",
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="ja">
-      <body>{children}</body>
-    </html>
-  );
-}
